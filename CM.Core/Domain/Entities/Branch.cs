b25ccb4c70910ddb305e39851873ec65f4681142
Manase <manasesmutembei11@gmail.com>
2024-07-18 16:38:57 +0300

    nullable values

--- CM.Core/Domain/Entities/Branch.cs
+++ CM.Core/Domain/Entities/Branch.cs
@@ -9,12 +9,12 @@ namespace CM.Core.Domain.Entities
     public class Branch
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
         public Guid CompanyId { get; set; }
 
         public Company Company { get; set; }
